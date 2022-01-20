46b8ad76c5eb903986265e44ae270d6e8399fc0f
ashaluwala kazeem <ashaluwalakazeem@gmail.com>
2022-01-20 20:34:09 +0100

    Miscellaneous update

--- periculum/src/main/java/com/periculum/models/VendorData.kt
+++ periculum/src/main/java/com/periculum/models/VendorData.kt
@@ -1,3 +1,10 @@
 package com.periculum.models
 
+/**
+ * @param phoneNumber
+ * @param bvn
+ * @param dti
+ * @param loanTenure
+ * @param token
+ */
 data class VendorData(val phoneNumber: String, val bvn: String, val dti: Double, val loanTenure: Int, val token: String)
